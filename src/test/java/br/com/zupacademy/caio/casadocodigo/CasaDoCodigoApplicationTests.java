52d85894832fe35c090154567604f67cada2ba06
zcaiobs <zcaiobernardodasilva@gmail.com>
2021-04-25 23:06:46 -0300

    ok

--- src/test/java/br/com/zupacademy/caio/casadocodigo/CasaDoCodigoApplicationTests.java
+++ src/test/java/br/com/zupacademy/caio/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -3,6 +3,7 @@ package br.com.zupacademy.caio.casadocodigo;
 import br.com.zupacademy.caio.casadocodigo.domain.Categoria;
 import br.com.zupacademy.caio.casadocodigo.repository.AutorRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,7 @@ class CasaDoCodigoApplicationTests {
 	EntityManager em;
 
 	@Test
+	@DisplayName("Deve retornar uma lista de categorias")
 	void existsCategoriaByNomeTest() {
 		String value = "Java";
 		String field = "nome";
